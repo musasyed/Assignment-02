7522d3590a51b15e22d2dcdbb68f2a81e69281fe
musasyed <musasyed16@gmail.com>
2023-06-11 18:33:15 +0500

    This is Modulus Feature

---
+++ modulus.py
@@ -0,0 +1,12 @@
+def calculate_modulus(a, b):
+    """
+    Calculates the modulus of two numbers.
+    
+    Parameters:
+        a (int): The first number.
+        b (int): The second number.
+    
+    Returns:
+        int: The modulus of a and b.
+    """
+    return a % b
